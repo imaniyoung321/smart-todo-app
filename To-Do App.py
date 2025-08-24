76b7344ba394802ca38987af9ec2c6fd45c5e357
Imani Young <imaniyoung321@gmail.com>
2025-08-24 10:59:37 -0400

    git push origin master

--- To-Do App.py
+++ To-Do App.py
@@ -1,12 +1,27 @@
 tasks = []
 
 while True:
-    task = input("Enter a task (or type 'quit' to stop):")
-    if task.lower() == "quit":
-        break
-    tasks.append(task)
-    print(f"Task '{task}' added!")
+    print("\nOptions:")
+    print("1. Add a task")
+    print("2. View tasks")
+    print("3. Quit")
 
-print("\nYour tasks:")
-for t in tasks:
-    print("-", t)
+    choice = input("Choose an option: ")
+
+    if choice == "1":
+        task = input("Enter a task: ")
+        tasks.append({"task": task, "done": False}) # store task and it's status
+        print(f"Task '{task}' added!")
+    elif choice == "2":
+        print("\nYour tasks:")
+        if not tasks:
+            print("No tasks yet.")
+        else:
+            for i, t in enumerate(tasks, start=1):
+                status = "*" if t["done"] else "X"
+                print(f"{i}. {t['task']} [{status}]")
+    elif choice == "3":
+        print("Goodbye!")
+        break
+    else:
+        print("Invalid choice, try again.")
